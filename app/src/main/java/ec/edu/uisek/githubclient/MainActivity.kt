79d29280802e35b25c442c43338227ba7223fa82
laboratorio 3.fs <riccardo.ceron@uisek.edu.ec>
2026-05-13 11:22:51 -0500

    Cambios 13-5-2026

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -4,20 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ec.edu.uisek.githubclient.ui.screens.RepoForm
 import ec.edu.uisek.githubclient.ui.screens.RepoList
 import ec.edu.uisek.githubclient.ui.theme.GithubClientTheme
+import ec.edu.uisek.githubclient.viewmodels.RepoListViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val listViewModel: RepoListViewModel=viewModel()
             GithubClientTheme {
-                RepoList()
+                var currentScreen by remember { mutableStateOf("repoList") }
+                when(currentScreen){
+                    "repoList" -> RepoList(
+                        onNavigateToForm = { currentScreen="repoForm" }
+                    )
+                    "repoForm" -> RepoForm(
+                        onSaveSuccess = {
+                            listViewModel.fetchRepos()
+                            currentScreen="repoList"},
+                        onBackClick = { currentScreen="repoList" }
+                    )
+                }
             }
         }
     }
